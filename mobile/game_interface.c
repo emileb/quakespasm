15942b4713fad534d5ad4c4d164dffcba4bafb18
Emile Belanger <emile.belanger@gmail.com>
2024-01-28 15:53:31 +0000

    Added run toggle

--- mobile/game_interface.c
+++ mobile/game_interface.c
@@ -323,11 +323,20 @@ void IN_Move_Android (usercmd_t *cmd)
     int blockMove = blockGamepad() & ANALOGUE_AXIS_FWD;
     int blockLook = blockGamepad() & ANALOGUE_AXIS_PITCH;
 
+	int fwdSpeed = 400;
+	int sideSpeed = 400;
+
+	int isPlayerRunning();
+	if(!isPlayerRunning())
+	{
+		fwdSpeed = fwdSpeed / 2;
+		sideSpeed = sideSpeed /2;
+	}
 
     if( !blockMove )
     {
-        cmd->sidemove += sidemove * 400;
-	    cmd->forwardmove += forwardmove * 400;
+        cmd->sidemove += sidemove * sideSpeed;
+	    cmd->forwardmove += forwardmove * fwdSpeed;
     }
 
     if( !blockLook )
